27a3644c4ac3d209951c8f479d82be7d93718952
gafasvvv <ry0suke.programming@gmail.com>
2019-02-21 16:28:52 +0900

    add Navbar lists

---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav>
+
+        <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">To do</span>
+                <span>Taskman</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <v-list>
+                <v-list-tile v-for='link in links' :key="link.text" router :to="link.route">
+                    <v-list-tile-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' },
+            ]
+        }
+    }
+}
+</script>
+
+
