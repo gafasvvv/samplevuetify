0fc523acd2e1f4e394a9719cddf72892ab885fec
gafasvvv <ry0suke.programming@gmail.com>
2019-02-22 21:55:15 +0900

    change views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption  text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption  text-lowercase">By person</span>
+        </v-btn>
+      </v-layout>
+      
+    <v-card flat v-for="project in projects" :key="project.title">
+      <v-layout row wrap :class="`pa-3 project ${project.status}`">
+        <v-flex xs12 md6>
+          <div class="caption grey--text">Project title</div>
+          <div>{{ project.title}}</div>
+        </v-flex>
+        <v-flex xs6  sm4 md2>
+          <div class="caption grey--text">Person</div>
+          <div>{{ project.person }}</div>
+        </v-flex>
+        <v-flex xs6  sm4 md2>
+          <div class="caption grey--text">Date</div>
+          <div>{{ project.date }}</div>
+        </v-flex>
+        <v-flex xs2  sm4 md2>
+          <div class="right">
+            <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-divider></v-divider>
+    </v-card>
+
+    </v-container>
+
+  </div>
+</template>
+
+<script> 
+
+export default {
+    data() {
+      return {
+        projects: [
+          { title: 'Design a new website', person: 'The Net Ninja', date: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Code up the homepage', person: 'Chun Li', date: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Design video thumbnails', person: 'Ryu', date: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+          { title: 'Create a community forum', person: 'Gouken', date: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        ]
+      }
+    },
+    methods: {
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+.project.complete {
+  border-left: 4px solid green;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background-color:  green;
+}
+.v-chip.ongoing {
+  background-color:  orange;
+}
+.v-chip.overdue {
+  background-color:  tomato;
+}
+</style>
---
+++ src/views/Team.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="team">
+    <h1 class="subheading grey--text">Team</h1>
+
+    <v-container class="my-5">
+        
+        <v-layout row>
+            <v-flex xs12 md6 wrap>
+                <v-btn outline block class="primary"></v-btn>
+            </v-flex>
+        </v-layout>
+
+    </v-container>
+    
+  </div>
+</template>
+
+<script> 
+
+export default {
+    
+}
+</script>
